bf554ba1a3bbccc10874824ee7e2d74d6fd73751
xunxxoie <ahh0520@naver.com>
2026-02-25 15:48:01 +0900

    feat: initial commit

---
+++ src/test/java/uss/code/UssQueueServerApplicationTests.java
@@ -0,0 +1,13 @@
+package uss.code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UssQueueServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
